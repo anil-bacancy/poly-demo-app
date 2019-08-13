c18fb91f39b235c5cecfa26b80648bc03db9f5b1
Anil Reddy <anil.reddy@bacancytechnology.com>
2019-08-13 16:04:08 +0530

    initial

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :photos do
+    resources :comments
+  end
+
+  resources :events do
+    resources :comments
+  end
+
+  resources :articles do
+    resources :comments
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+end
